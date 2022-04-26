a0e33eaa8a79e7a6858a76efff6337fa5520d245
bahsz <sebasdevc78@gmail.com>
2022-04-26 02:18:48 -0300

    agregando rutas

---
+++ app.js
@@ -0,0 +1,23 @@
+// Express - Path
+const express = require('express');
+const path = require('path');
+
+// APP
+const app = express();
+
+// Views - Html
+const indexHtml = path.join(__dirname, 'views/index.html')
+
+// Public
+const publicPath = path.join(__dirname, 'public/');
+
+// Puertos
+app.listen(3000,()=>{
+    console.log('SyntaxError');
+});
+
+app.use(express.static(publicPath));
+
+app.get('/', (req,res) => {
+    res.sendFile(indexHtml)
+})
