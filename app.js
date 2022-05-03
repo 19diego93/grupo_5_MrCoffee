fa1ead63c9a5d098e340a4fe2825a6b8c8961765
bahsz <sebasdevc78@gmail.com>
2022-05-02 21:15:14 -0300

    arreglando errores

--- app.js
+++ app.js
@@ -1,29 +1,33 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
 const port = 3000;
 
 // Views
-const viewsIndex = path.join(__dirname, 'views/index.html')
-const viewsRegister = path.join(__dirname,'views/register.html')
-const viewsLogin = path.join(__dirname, 'views/login.html')
+const viewsIndex = path.join(__dirname, "views/index.html");
+const viewsRegister = path.join(__dirname, "views/register.html");
+const viewsLogin = path.join(__dirname, "views/login.html");
+const viewsPepe = path.join(__dirname, "views/pepe.html");
 // Static
-const publicPath = path.join(__dirname, 'public/');
+const publicPath = path.join(__dirname, "public/");
 app.use(express.static(publicPath));
 
 // Puertos
-app.listen(port,()=>{
-    console.log(`MrCoffee listening at http://localhost:${port}`)
-})
+app.listen(port, () => {
+  console.log(`MrCoffee listening at http://localhost:${port}`);
+});
 
-app.get('/', (req,res) => {
-    res.sendFile(viewsIndex)
-})
+app.get("/", (req, res) => {
+  res.sendFile(viewsIndex);
+});
 
-app.get('/register',(req,res) => {
-    res.sendFile(viewsRegister)
-})
+app.get("/register", (req, res) => {
+  res.sendFile(viewsRegister);
+});
 
-app.get('/login',(req,res) => {
-    res.sendFile(viewsLogin)
-})
+app.get("/login", (req, res) => {
+  res.sendFile(viewsLogin);
+});
+app.get("/pepe", (req, res) => {
+  res.sendFile(viewsPepe);
+});
