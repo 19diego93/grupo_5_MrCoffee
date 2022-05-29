fb5062572ae591ea40204813c050ae948045d713
bahsz <sebasdevc78@gmail.com>
2022-05-29 15:55:18 -0300

    acomodando hojas de estilo

--- site/src/routes/products/products.js
+++ site/src/routes/products/products.js
@@ -10,6 +10,6 @@ router.get("/", productsCrontroller.productShop);
 
 router.get("/cart", productsCrontroller.productCart);
 
-router.get("/detail", productsCrontroller.productDetail);
+router.get("/detail/:id", productsCrontroller.productDetail);
 
 module.exports = router;
