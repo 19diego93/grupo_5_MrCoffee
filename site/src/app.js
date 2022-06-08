5646ad005dba91db04fddc7461aa1672a576f5de
bahsz <sebasdevc78@gmail.com>
2022-06-08 20:16:36 -0300

    agregando multer y not-found.ejs

--- site/src/app.js
+++ site/src/app.js
@@ -7,6 +7,12 @@ const app = express();
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
 
+//' Middlewares
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+const methodOverride = require("method-override");
+app.use(methodOverride("_method"));
+
 //' Static
 app.use(express.static("public"));
 
@@ -22,11 +28,12 @@ const adminRouter = require("./routes/admin/admin.js");
 const productsRouter = require("./routes/products/products.js");
 
 //' Puertos
-//! home
-app.use("/", mainRouter);
-//! users.js
-app.use("/user", usersRouter);
-//! admin.js
-app.use("/admin", adminRouter);
-//! productos
-app.use("/product", productsRouter);
+app.use("/", mainRouter); //! home
+app.use("/user", usersRouter); //! users.js
+app.use("/admin", adminRouter); //! admin.js
+app.use("/product", productsRouter); //! productos
+
+//' 404
+app.use((req, res, next) => {
+  res.status(404).render("not-found");
+});
