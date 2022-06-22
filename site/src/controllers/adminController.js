15543ccffb05875e6ecce8f47a98ea12d821498d
bahsz <sebasdevc78@gmail.com>
2022-06-21 22:25:36 -0300

    Creacion de productos

--- site/src/controllers/adminController.js
+++ site/src/controllers/adminController.js
@@ -5,10 +5,35 @@ const productsFilePath = path.join(__dirname, "../data/productsDataBase.json");
 const products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8"));
 
 const adminController = {
-  createProducts: (req, res) => {
-    res.render("admin/createProducts");
+  viewCreate: (req, res) => {
+    res.render("admin/create");
   },
-  editProducts: (req, res) => {
+  create: (req, res) => {
+    allProducts = products;
+
+    let image = "default-image.png";
+
+    if (req.file != undefined) {
+      image = req.file.filename;
+    }
+
+    nuevoProduct = {
+      id: Date.now() + Date.now() * 100,
+      name: req.body.name,
+      image: image,
+      description: req.body.description,
+      category: [req.body.category, req.body.category2],
+      stock: parseInt(req.body.stock),
+      price: parseInt(req.body.price),
+      offer: parseInt(req.body.offer),
+    };
+
+    allProducts.push(nuevoProduct);
+    let guardar = JSON.stringify(allProducts, null, 2);
+    fs.writeFileSync(productsFilePath, guardar, "utf-8");
+    res.redirect("/");
+  },
+  viewEdit: (req, res) => {
     res.render("admin/editProducts", { products });
   },
 };
--- site/src/routes/admin/admin.js
+++ site/src/routes/admin/admin.js
@@ -1,13 +1,25 @@
 const express = require("express");
 const router = express.Router();
 const path = require("path");
+const multer = require("multer");
 const adminController = require(path.join(
   __dirname,
   "/../../controllers/adminController"
 ));
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, "../../../public/img/products"));
+  },
+  filename: (req, file, cb) => {
+    const newFile = "img-" + Date.now() + path.extname(file.originalname);
+    cb(null, newFile);
+  },
+});
+const upload = multer({ storage });
 
-router.get("/create", adminController.createProducts);
+router.get("/create", adminController.viewCreate);
+router.post("/", upload.single("image"), adminController.create);
 
-router.get("/edit", adminController.editProducts);
+router.get("/edit", adminController.viewEdit);
 
 module.exports = router;
