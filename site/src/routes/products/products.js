4d2cad1f8e405b0ef498fdbde6bca8e0bdcff841
bahsz <sebasdevc78@gmail.com>
2022-06-22 13:20:18 -0300

    sacando edit de productController

--- site/src/routes/products/products.js
+++ site/src/routes/products/products.js
@@ -2,16 +2,17 @@ const express = require("express");
 const router = express.Router();
 const path = require("path");
 
-const multer = require('multer');
-const storage = multer.diskStorage({destination:(req,file,cb)=>{
-  cb(null,path.join(__dirname,`../../../public/img`))
-},
-  filename:(req,file,cb)=>{
-    const newFileName = "cafe"+ Date.now() + path.extname(file.originalname);
-    cb(null, newFileName )
-  }
+const multer = require("multer");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, path.join(__dirname, `../../../public/img`));
+  },
+  filename: (req, file, cb) => {
+    const newFileName = "cafe" + Date.now() + path.extname(file.originalname);
+    cb(null, newFileName);
+  },
 });
-const upload = multer({storage: storage})
+const upload = multer({ storage: storage });
 
 const productsCrontroller = require(path.join(
   __dirname,
@@ -23,9 +24,5 @@ router.get("/", productsCrontroller.productShop);
 router.get("/cart/:id?", productsCrontroller.productCart);
 
 router.get("/detail/:id", productsCrontroller.productDetail);
-
-router.get("/edit/:id", productsCrontroller.edit)
-
-router.put("/:id",upload.single('image'), productsCrontroller.edited)
 
 module.exports = router;
