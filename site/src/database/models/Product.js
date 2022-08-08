177559e54a787ee4b990ea9499f6394d9ea8bb53
diego vallejo <vallejodiego@live.com>
2022-08-07 23:45:05 -0300

    configuracion modelos sequelize

---
+++ site/src/database/models/Product.js
@@ -0,0 +1,54 @@
+module.exports = (sequelize, dataTypes) => {
+    let alias = 'Product'; // esto debería estar en singular
+    let cols = {
+        id: {
+            type: dataTypes.BIGINT(13),
+            primaryKey: true,
+            allowNull: false,
+            autoIncrement: false
+        },
+       
+        name: {
+            type: dataTypes.STRING(40),
+            allowNull: false
+        },
+        image: {
+            type: dataTypes.STRING(21),
+            allowNull: true
+        },
+        description: {
+            type: dataTypes.STRING(120),
+            allowNull: true
+        },
+        stock: {
+            type: dataTypes.INTEGER(5),
+            allowNull: true
+        },
+        price: {
+            type: dataTypes.DECIMAL(6,2),
+            allowNull: false
+        },
+        offer: {
+            type: dataTypes.INTEGER(2),
+            allowNull: true
+        },
+        rating: {
+            type: dataTypes.DECIMAL(2,1),
+            allowNull: true
+        },
+        
+    };
+    let config = {
+        timestamps: false,
+        }
+    const Product = sequelize.define(alias,cols,config);
+
+    Product.associate = function (models) {
+        Product.belongsTo(models.Product_category, { // models.Product_category -> Product_category es el valor de alias en Product_category.js
+            as: "product_category",
+            foreignKey: "id_categoryP"
+        })
+    }
+
+    return Product
+};
