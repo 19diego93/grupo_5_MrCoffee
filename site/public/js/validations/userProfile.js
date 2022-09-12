39958761425f257f1ca17554be19ed43ce571569
bahsz <sebasdevc78@gmail.com>
2022-09-12 00:44:46 -0300

    validacion front password

--- site/public/js/validations/userProfile.js
+++ site/public/js/validations/userProfile.js
@@ -10,8 +10,7 @@ window.addEventListener("load", function () {
       lower: new RegExp("(?=.*[a-z])"),
       upper: new RegExp("(?=.*[A-Z])"),
       number: new RegExp("(?=.*[0-9])"),
-      special: new RegExp("(?=.*[@#$%^&*])"),
-      specialError: new RegExp("(?=.*[!-])"),
+      special: new RegExp("(?=.*[!@#$%^&*])"),
     },
   };
 
@@ -20,8 +19,6 @@ window.addEventListener("load", function () {
     last_name: true,
     email: true,
     oldPassword: false,
-    newPassword: true,
-    confirmNewPassword: true,
   };
 
   const validarFormulario = (e) => {
@@ -37,13 +34,6 @@ window.addEventListener("load", function () {
         break;
       case "oldPassword":
         validationPassword(expresiones.password, e.target, "oldPassword");
-        break;
-      case "newPassword":
-        validationPassword(expresiones.password, e.target, "newPassword");
-        validationPassword_2();
-        break;
-      case "confirmNewPassword":
-        validationPassword_2();
         break;
     }
   };
@@ -115,191 +105,6 @@ window.addEventListener("load", function () {
       inputColor.classList.add("is-valid");
       inputColor.classList.add("is-valid-icon");
       campos[campo] = true;
-    }
-  };
-
-  const validationPassword = (expresion, input, campo) => {
-    const inputPassword1 = document.getElementById("oldPassword");
-    const inputPassword2 = document.getElementById("newPassword");
-
-    const errorsMessage = document.querySelectorAll(`.${campo}Errors`);
-    const inputColor = document.getElementById(`${campo}`);
-
-    if (input.value.length === 0) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      if (campo === "newPassword") {
-        inputColor.classList.remove("is-invalid");
-        inputColor.classList.add("is-valid");
-        inputColor.classList.remove("is-invalid-icon");
-        inputColor.classList.add("is-valid-icon");
-
-        campos[campo] = true;
-      } else {
-        inputColor.classList.remove("is-valid");
-        inputColor.classList.add("is-invalid");
-        inputColor.classList.remove("is-valid-icon");
-        inputColor.classList.add("is-invalid-icon");
-
-        document.querySelector(`.${campo}Vacio`).classList.add("displayBlock");
-        campos[campo] = false;
-      }
-    } else if (
-      inputPassword2.value.length > 0 &&
-      inputPassword1.value.length === 0
-    ) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document
-        .querySelector(`.newPasswordPass`)
-        .classList.remove("displayNone");
-      document.querySelector(`.newPasswordPass`).classList.add("displayBlock");
-      document
-        .querySelector(`.oldPasswordPass`)
-        .classList.remove("displayNone");
-      document.querySelector(`.oldPasswordPass`).classList.add("displayBlock");
-
-      inputPassword1.classList.remove("is-valid");
-      inputPassword1.classList.add("is-invalid");
-      inputPassword1.classList.remove("is-valid");
-      inputPassword1.classList.add("is-invalid");
-      campos["oldPassword"] = false;
-    } else if (input.value.length < 8) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-      if (campo === "oldPassword") {
-        document.querySelector(`.newPasswordPass`).classList.add("displayNone");
-        document
-          .querySelector(`.newPasswordPass`)
-          .classList.remove("displayBlock");
-      }
-      console.log(expresion.lower);
-
-      document.querySelector(`.${campo}Min`).classList.add("displayBlock");
-
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-      campos[campo] = false;
-    } else if (input.value.length > 65) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document.querySelector(`.${campo}Max`).classList.add("displayBlock");
-
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-
-      campos[campo] = false;
-    } else if (!expresion.lower.test(input.value)) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document.querySelector(`.${campo}Lower`).classList.add("displayBlock");
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-
-      campos[campo] = false;
-    } else if (!expresion.upper.test(input.value)) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document.querySelector(`.${campo}upper`).classList.add("displayBlock");
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-
-      campos[campo] = false;
-    } else if (!expresion.number.test(input.value)) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document.querySelector(`.${campo}Number`).classList.add("displayBlock");
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-
-      campos[campo] = false;
-    } else if (!expresion.special.test(input.value)) {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      document.querySelector(`.${campo}Special`).classList.add("displayBlock");
-      inputColor.classList.remove("is-valid");
-      inputColor.classList.add("is-invalid");
-      inputColor.classList.remove("is-valid-icon");
-      inputColor.classList.add("is-invalid-icon");
-
-      campos[campo] = false;
-    } else {
-      errorsMessage.forEach((error) => {
-        error.classList.remove("displayBlock");
-        error.classList.add("displayNone");
-      });
-
-      inputColor.classList.remove("is-invalid");
-      inputColor.classList.add("is-valid");
-      inputColor.classList.remove("is-invalid-icon");
-      inputColor.classList.add("is-valid-icon");
-      campos[campo] = true;
-    }
-  };
-
-  const validationPassword_2 = () => {
-    const inputPassword1 = document.getElementById("newPassword");
-    const inputPassword2 = document.getElementById("confirmNewPassword");
-
-    if (inputPassword1.value !== inputPassword2.value) {
-      inputPassword2.classList.remove("is-valid");
-      inputPassword2.classList.add("is-invalid");
-      inputPassword2.classList.remove("is-valid-icon");
-      inputPassword2.classList.add("is-invalid-icon");
-      campos["confirmNewPassword"] = false;
-
-      document
-        .querySelector(".confirmNewPasswordEq")
-        .classList.remove("displayNone");
-      document
-        .querySelector(".confirmNewPasswordEq")
-        .classList.add("displayBlock");
-    } else {
-      inputPassword2.classList.remove("is-invalid");
-      inputPassword2.classList.add("is-valid");
-      inputPassword2.classList.remove("is-invalid-icon");
-      inputPassword2.classList.add("is-valid-icon");
-      campos["confirmNewPassword"] = true;
-
-      document
-        .querySelector(".confirmNewPasswordEq")
-        .classList.remove("displayBlock");
-      document
-        .querySelector(".confirmNewPasswordEq")
-        .classList.add("displayNone");
     }
   };
 
